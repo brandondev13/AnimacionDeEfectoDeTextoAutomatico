735c407e054f86665226f93e5cda280f59e73e57
Brandon Mejia <brandonmejiarestrepo@gmail.com>
2022-12-04 12:54:17 -0500

    Dandole interactividad con javaScript

---
+++ Proyecto/index.js
@@ -0,0 +1,19 @@
+const containerEl = document.querySelector(".container")
+const careers = [ "COMPETIDOR ICPC", "BACKEND DEVELOPER", "FRONTEND DEVELOPER", "ANDROID DEVELOPER", "FREELANCER", "YOUTUBER" ];
+let careerIndex = 0
+let characterIndex = 0
+updateText();
+function updateText() {
+    characterIndex++;
+    containerEl.innerHTML =  `<h1>I AM ${careers[careerIndex].slice(0, 1) === "I" || careers[careerIndex].slice(0, 1) == "A"  ? "AN" : "A"}
+    ${careers[careerIndex].slice(0, characterIndex)}</h1>`;    
+    if(characterIndex === careers[careerIndex].length) {
+        careerIndex++
+        characterIndex = 0
+    }
+    if(careerIndex === careers.length) {
+        careerIndex = 0;
+    }
+    setTimeout(updateText, 400)
+}
+
